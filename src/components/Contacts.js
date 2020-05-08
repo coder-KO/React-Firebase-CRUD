d10ba91299965193de858c72192c0f1a360d9e8d
coder-KO <drkaojha@gmail.com>
2020-05-09 01:11:34 +0530

    data retrival from firebase db established

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ContactForm from "./ContactForm";
 import firebaseDb from "../firebase_auth";
 
 const Contacts = () => {
+  var [contactObjects, setcontactObjects] = useState({});
+
+  useEffect(() => {
+    firebaseDb.child("Contacts").on("value", (snapshot) => {
+      if (snapshot.val() != null)
+        setcontactObjects({
+          ...snapshot.val(),
+        });
+    });
+  });
   var userID = 200019993200;
 
   const addOrEdit = (obj) => {
-    firebaseDb.ref("Contacts/" + userID).set(obj, (err) => {
+    firebaseDb.child("Contacts").push(obj, (err) => {
       if (err) console.log(err);
     });
     userID += 1;
@@ -14,9 +24,9 @@ const Contacts = () => {
 
   return (
     <>
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4 text-center">Contact Register</h1>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-center">Contact Register</h1>
         </div>
       </div>
 
@@ -25,7 +35,35 @@ const Contacts = () => {
           <ContactForm addOrEdit={addOrEdit} />
         </div>
         <div className="col-md-7">
-          <div>List of Contacts</div>
+          <table className="table table-borderless table-stripped">
+            <thead className="thead-light">
+              <tr>
+                <th>Full Name</th>
+                <th>Mobile</th>
+                <th>E-Mail</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(contactObjects).map((id) => {
+                return (
+                  <tr key={id}>
+                    <td>{contactObjects[id].fullName}</td>
+                    <td>{contactObjects[id].mobile}</td>
+                    <td>{contactObjects[id].email}</td>
+                    <td>
+                      <button className="btn text-primary">
+                        <i className="fas fa-pencil-alt"></i>
+                      </button>
+                      <button className="btn text-danger">
+                        <i className="fas fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
