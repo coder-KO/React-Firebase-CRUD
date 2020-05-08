4fd02f5291391b4737140256da23abe1b4d585a9
coder-KO <drkaojha@gmail.com>
2020-05-08 23:38:53 +0530

    created form components

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 const ContactForm = () => {
+  const initailFieldValues = {
+    fullName: "",
+    mobile: "",
+    email: "",
+    address: "",
+  };
 
-const initailFieldValues = {
-    fullName = '',
-    mobile = '',
-    email = '',
-    address = ''
-}
+  var [values, setValues] = useState(initailFieldValues);
 
-var [values, setValues] = useState(initailFieldValues)
-
-  return <div>Display Form</div>;
+  return (
+    <form autoComplete="off">
+      <div className="form-group input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">
+            <i className="fas fa-user"></i>
+          </div>
+        </div>
+        <input
+          className="form-control"
+          placeholder="Full Name"
+          name="fullName"
+          value={values.fullName}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
